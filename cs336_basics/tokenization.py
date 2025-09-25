f525d1db11dbf9d4f08ebc1de42fa764d674ae68
Emilio Cantú <emiliocantu.500@gmail.com>
2025-09-25 12:21:08 -0400

    cleanup by using find_chunk_boundaries

--- cs336_basics/tokenization.py
+++ cs336_basics/tokenization.py
@@ -1,14 +1,44 @@
 import os
 import regex as re
 from collections import Counter
+from typing import BinaryIO
 
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
+def pretokenize(input_path: str | os.PathLike, special_tokens: list[str], chunk_size: int, end_of_doc_token: str) -> dict[tuple[bytes], int]:
+
+    # Pretokenize by scanning the whole file
+    pretokenized: dict[tuple[bytes], int] = Counter()
+    special_pat = "|".join(re.escape(t) for t in sorted(special_tokens, key=len, reverse=True))
+
+
+    file_size = os.path.getsize(input_path)
+    N_CHUNKS = (file_size + chunk_size - 1) // chunk_size
+
+    with open(input_path, "rb") as f:
+
+        boundaries = find_chunk_boundaries(f, N_CHUNKS, end_of_doc_token.encode())
+
+        # TODO parallelize this by sending each start/end pair to a set of processes.
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+
+            parts = re.split(special_pat, chunk) if special_tokens else [chunk]
+            for part in parts:
+                for m in re.finditer(PAT, part):
+                    s = m.group(0)
+                    b = s.encode("utf-8")
+                    pretokenized[tuple(b[j:j+1] for j in range(len(b)))] += 1
+
+    return pretokenized
 
 def train_bpe(
     input_path: str | os.PathLike,
     vocab_size: int,
     special_tokens: list[str],
+    end_of_doc_token: str = "<|endoftext|>",
+    chunk_size: int = 8192,
     **kwargs,
 ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
     """Given the path to an input corpus, run train a BPE tokenizer and
@@ -21,6 +51,11 @@ def train_bpe(
             These strings will never be split into multiple tokens, and will always be
             kept as a single token. If these special tokens occur in the `input_path`,
             they are treated as any other string.
+        end_of_doc_token (str, optional): A special token that indicates the end of a document.
+            This token is used to find chunk boundaries when splitting the input file into chunks.
+            Defaults to "<|endoftext|>".
+        chunk_size (int, optional): The size (in bytes) of each chunk to read from the input file.
+            Defaults to 8192 (8kB).
 
     Returns:
         tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
@@ -36,46 +71,26 @@ def train_bpe(
     vocab = [bytes([i]) for i in range(256)] + [t.encode() for t in special_tokens]
     merges = []
 
-    # Pretokenize by scanning the whole file
-    pretokenized: dict[tuple[bytes], int] = Counter()
-    special_pat = "|".join(re.escape(t) for t in sorted(special_tokens, key=len, reverse=True))
+    pretokenized = pretokenize(input_path, special_tokens, chunk_size, end_of_doc_token)
 
-    # TODO make this more efficient. Currently just adding last part to carry every time. 
-    # What if last part is huge?
-    CHUNK = 8192
-    carry = ""
-    with open(input_path, "r", encoding="utf-8") as f:
-        while True:
-
-            chunk = f.read(CHUNK)
-            txt = carry + chunk
-            if not txt:
-                break
-
-            carry = ""
-
-            parts = re.split(special_pat, txt) if special_tokens else [txt]
-
-            for i, part in enumerate(parts):
-                if i == len(parts) - 1 and chunk:
-                    carry = part + carry
-                    break
-                for m in re.finditer(PAT, part):
-                    s = m.group(0)
-                    b = s.encode("utf-8")
-                    pretokenized[tuple(b[j:j+1] for j in range(len(b)))] += 1
-
-    # Build pair frequencies
     pair_freq = Counter()
-    for bs, freq in pretokenized.items():
-        for i in range(0, len(bs) - 1):
-            k = bs[i : i + 2]
-            pair_freq[k] += freq
+    
+    def get_pair_freq(): # Function for profiling
+        # Build pair frequencies
+        for bs, freq in pretokenized.items():
+            for i in range(0, len(bs) - 1):
+                k = bs[i : i + 2]
+                pair_freq[k] += freq
+    get_pair_freq()
 
-    while len(vocab) < vocab_size:
-        most_freq = max(
+    def get_most_freq():
+        return max(
             pair_freq, key=lambda x: (pair_freq[x], *x)
         )  # clever way to break ties: second criterion as second item in tuple
+
+    while len(vocab) < vocab_size:
+        
+        most_freq = get_most_freq()
         del pair_freq[most_freq]
 
         a, b = most_freq
@@ -124,6 +139,55 @@ def train_bpe(
     return {i: b for i, b in enumerate(vocab)}, merges
 
 
+# Taken verbatim from pretokenization_example.py
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
+
+
+
 if __name__ == "__main__":
     # input_path = "data/TinyStoriesV2-GPT4-valid.txt"
     # input_path = "data/tokenizer_test.txt"
@@ -134,13 +198,19 @@ if __name__ == "__main__":
     # print(f"vocab size: {len(vocab)}")
     # assert merges[-1] == (b"lowe", b"r")
 
+    import cProfile
+
     input_path = "tests/fixtures/tinystories_sample_5M.txt"
-    vocab, merges = train_bpe(
-        input_path=input_path,
-        vocab_size=1000,
-        special_tokens=["<|endoftext|>"],
+
+    # input_path = "data/TinyStoriesV2-GPT4-valid.txt"
+    # vocab, merges = train_bpe(
+    #     input_path=input_path,
+    #     vocab_size=10_000,
+    #     special_tokens=["<|endoftext|>"],
+    # )
+
+    cProfile.run(
+        "train_bpe(input_path=input_path, vocab_size=10_000, special_tokens=['<|endoftext|>'])"
     )
 
-    assert merges[-1] == (b' mag', b'ic')
-    print(merges[-1])
-    # 628
+    # import 
