36f010b27fb70c13b0e4b2a46576f8de4aff0530
Emilio Cantú <emiliocantu.500@gmail.com>
2025-09-25 11:25:04 -0400

    train_bpe passes

--- cs336_basics/tokenization.py
+++ cs336_basics/tokenization.py
@@ -3,14 +3,8 @@ import regex as re
 from collections import Counter
 
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-
-
-def pretokenize(input_path: str | os.PathLike) -> list[bytes]:
-    re.finditer(
-        "\s",
-    )
-
-    raise NotImplementedError
+# just split on whitespace
+# PAT = r"""\S+"""
 
 
 def train_bpe(
@@ -41,102 +35,110 @@ def train_bpe(
                 Merges are ordered by order of creation.
     """
 
-    # Pretokenize by scanning the whole file
-    pretokenized: dict[bytes, int] = Counter()
+    vocab = [bytes([i]) for i in range(256)] + [t.encode() for t in special_tokens]
+    merges = []
 
-    CHUNK = 8192  # read 8 KB at a time TODO: tune?
+    # Pretokenize by scanning the whole file
+    pretokenized: dict[tuple[bytes], int] = Counter()
+    special_set = {t.encode() for t in special_tokens}
+    escaped_specials = [re.escape(t).encode() for t in special_tokens]
+    escaped_specials.sort(key=len, reverse=True)
+    token_pattern_bytes = PAT.encode()
+    if escaped_specials:
+        special_branch = b"|".join(escaped_specials)
+        token_pattern_bytes = b"(?:" + special_branch + b")|" + token_pattern_bytes
+    token_pattern = re.compile(token_pattern_bytes)
+
+    CHUNK = None #8192  # read 8 KB at a time TODO: tune?
     with open(input_path, "rb") as f:
         while True:
             txt = f.read(CHUNK)
             if not txt:
                 break
 
-            for m in re.finditer(PAT.encode(), txt):
-                pretokenized[tuple(m[0])] += 1
+            for m in token_pattern.finditer(txt):
+                token_bytes = m[0]
+                if token_bytes in special_set:
+                    pretokenized[(token_bytes,)] += 1
+                    continue
+
+                k = tuple(token_bytes[i : i + 1] for i in range(len(token_bytes)))
+                pretokenized[k] += 1
+    
+    # print('initial pretokenized', pretokenized)
+    for bs in pretokenized:
+        assert r'<' not in bs
 
     # Build pair frequencies
-    pair_freq: dict[tuple[bytes, bytes], int] = Counter()
+    pair_freq = Counter()
     for bs, freq in pretokenized.items():
         for i in range(0, len(bs)-1):
-            pair_freq[bs[i:i+2]] += freq
+            k = bs[i:i+2]
+            pair_freq[k] += freq
 
+    # print(len(pretokenized), len(pair_freq))
+    # print('='*40)
 
-    print(pretokenized.most_common(10))
-    print(pair_freq.most_common(10))
-    print(len(pretokenized), len(pair_freq))
+    while len(vocab) < vocab_size:
 
-    merges = []
+        most_freq = max(pair_freq, key=lambda x: (pair_freq.get(x, 0), *x)) # clever way to break ties: second criterion as second item in tuple
+        del pair_freq[most_freq]
 
-    (most_freq, highest_pair_freq), = pair_freq.most_common(1)
-    a, b = most_freq
-    a_t = a if isinstance(a, tuple) else (a,)
-    b_t = b if isinstance(b, tuple) else (b,)
+        a, b = most_freq
+        # print(a, b)
 
-    merges.append(most_freq)
+        merges.append(most_freq)
+        vocab.append(a+b)
 
-    print('most freq', most_freq, freq, a, b)
-
-    for _ in range(10):#(vocab_size - 256 - len(special_tokens)):
-
+        # in the pretokenized dict, replace keys (..., a, b, ...) with (..., ab, ...)
         for bs in list(pretokenized.keys()):
+
+            pre_freq = pretokenized.get(bs, 0)
 
             i = 0
             while i < len(bs)-1:
                 if bs[i:i+2] == most_freq:
 
                     # replace (..., a, b, ...) with (..., ab, ...)
-                    new_bs = bs[:i] + (a_t+b_t,) + bs[i+2:]
-                    pre_freq = pretokenized.pop(bs)
-
-                    print('new bs', bs, new_bs)
+                    new_bs = bs[:i] + (a+b,) + bs[i+2:]
+                    pretokenized[new_bs] = pre_freq
+                    del pretokenized[bs]
 
                     # update pair_freq
-                    m = (*a_t, *b_t),
 
                     # add (prev, (a,b)) and ((a,b), next)
                     if i > 0:
-                        prev = bs[i-1:i]
-                        pair_freq[prev + m] += pre_freq
+                        pair_freq[bs[i-1], a+b] += pre_freq
 
                     if i < len(bs)-2:
-                        next = bs[i+2:i+3]
-                        pair_freq[m + next] += pre_freq
+                        pair_freq[a+b, bs[i+2]] += pre_freq
 
                     # remove (prev, a), (b, next)
                     if i > 0:
-                        prev = bs[i-1:i]
-                        k = prev + (a,)
+                        k = (bs[i-1], a)
                         pair_freq[k] -= pre_freq
                         if pair_freq[k] <= 0:
                             del pair_freq[k]
                     
                     if i < len(bs)-2:
-                        next = bs[i+2:i+3]
-                        k = (b,) + next
+                        k = (b, bs[i+2])
                         pair_freq[k] -= pre_freq
                         if pair_freq[k] <= 0:
                             del pair_freq[k]
-                
+                    
                     bs = new_bs
                 else:
                     i += 1
-
-
-    import pdb; pdb.set_trace()
-
-    # break
+    
+    return {i: b for i, b in enumerate(vocab)}, merges
 
 
 if __name__ == "__main__":
-    input_path = "data/TinyStoriesV2-GPT4-valid.txt"
-    vocab_size = 50
-    special_tokens = ["<pad>", "<unk>", "<s>", "</s>"]
+    # input_path = "data/TinyStoriesV2-GPT4-valid.txt"
+    input_path = "data/tokenizer_test.txt"
+    special_tokens = ["<pad>", "<unk>", "<s>", "</s>", "<|endoftext|>"]
+    vocab_size = 256 + len(special_tokens) + 12
 
-    train_bpe(input_path, vocab_size, special_tokens)
-
-    # print('vocab:')
-    # for i, b in vocab.items():
-    #     print(f'  {i:3}: {b}')
-    # print('merges:')
-    # for a, b in merges:
-    #     print(f'  {bytes(a)} {bytes(b)}')
+    vocab, merges = train_bpe(input_path, vocab_size, special_tokens)
+    print(f'vocab size: {len(vocab)}')
+    assert merges[-1] == (b'lowe', b'r')
