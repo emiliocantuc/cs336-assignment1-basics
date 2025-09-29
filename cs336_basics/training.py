8c3e5f441602844023283b3dbf87dfa48e8abd51
Emilio Cantú <emiliocantu.500@gmail.com>
2025-09-29 13:34:27 -0400

    training utils

---
+++ cs336_basics/training.py
@@ -0,0 +1,40 @@
+import numpy as np
+import torch
+import os
+from typing import BinaryIO, IO
+
+
+def get_batch(x: np.array, batch_size: int, context_length: int, device: str):
+    # clever: use broadcasting to create the input and target sequences
+    # and avoid appending to lists and then stacking
+
+    starts = np.random.choice(np.arange(len(x) - context_length), size=batch_size, replace=False)
+    offsets = np.arange(context_length)
+
+    ixs = starts[:, None] + offsets  # (batch_size, context_length)
+    inputs = torch.as_tensor(x[ixs], dtype=torch.long, device=device)
+    targets = torch.as_tensor(x[ixs + 1], dtype=torch.long, device=device)
+    return inputs, targets
+
+
+def save_checkpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    iteration: int,
+    out: str | os.PathLike | BinaryIO | IO[bytes],
+):
+    checkpoint = {
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "iteration": iteration,
+    }
+    torch.save(checkpoint, out)
+
+
+def load_checkpoint(
+    src: str | os.PathLike | BinaryIO | IO[bytes], model: torch.nn.Module, optimizer: torch.optim.Optimizer
+):
+    checkpoint = torch.load(src)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+    return checkpoint["iteration"]
